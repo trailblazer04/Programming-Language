f85e0da70216029568d320dcc3a647c90ddb657c
trailblazer04 <leofrancis6988@gmail.com>
2025-12-29 17:11:19 +0530

    Create a variable inside a function, with the same name as the global variable

--- Python/Fundamental/variables/global-variables.py
+++ Python/Fundamental/variables/global-variables.py
@@ -1,11 +1,14 @@
 # Global variables
-
+# Create a variable inside a function, with the same name as the global variable
 x = "awesome"
 
 def myfunc():
+    x = "fantastic"
     print("python is " + x)
 
 myfunc()
+
+print("Python is " + x)
 
 
 
