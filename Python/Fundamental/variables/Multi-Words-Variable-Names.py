daf0e29f2f5d626e5f47986ab92dd266ecd6e1ff
trailblazer04 <leofrancis6988@gmail.com>
2025-12-29 16:57:31 +0530

    Leaned about Python - Variable Names and Multi Words Variable Names

---
+++ Python/Fundamental/variables/Multi-Words-Variable-Names.py
@@ -0,0 +1,18 @@
+# Multi Words Variable Names
+
+# Camel Case
+
+myVariableName = "John"
+
+print(myVariableName)
+# Pascal Case
+
+MyVariableName = "John"
+
+print(MyVariableName)
+
+# Snake Case
+
+my_variable_name = "John"
+
+print(my_variable_name)
